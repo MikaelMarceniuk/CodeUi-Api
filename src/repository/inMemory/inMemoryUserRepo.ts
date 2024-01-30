cec24102c3077bcee6acbc1480e432ddbfb3e98e
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-01-30 00:27:24 -0300

    test: Implemented inMemoryPostgres and unitTest for UpdateUserUseCase

---
+++ src/repository/inMemory/inMemoryUserRepo.ts
@@ -0,0 +1,45 @@
+import InMemoryPostgresql from "@libs/inMemoryPostgres";
+import { Prisma, User } from "@prisma/client";
+import IUserRepository from "@repository/IUserRepository";
+import { randomUUID } from "node:crypto";
+
+class InMemoryUserRepo implements IUserRepository {
+  async findById(id: string) {
+    const dbUser = await InMemoryPostgresql.getInstance().public.one(`select * from "User" where "User".id = '${id}'`)
+    return dbUser || null
+  }
+
+  async findByEmail(email: string) {
+    const dbUser = await InMemoryPostgresql.getInstance().public.one(`select * from "User" where "User".email = '${email}'`)
+    return dbUser || null
+  }
+
+  async save(data: Prisma.UserCreateInput): Promise<User> {
+    const uuid = randomUUID()
+
+    await InMemoryPostgresql.getInstance().public.query(
+      `insert into "User" (id, username, email, password_hash, created_at) values ('${uuid}', '${data.username}', '${data.email}', '${data.password_hash}', '${new Date().toJSON()}')`
+    )
+
+    return await InMemoryPostgresql.getInstance().public.one(`select * from "User" where "User".id = '${uuid}'`)
+  }
+
+  async update(id: string, data: Prisma.UserUncheckedUpdateInput) {
+    await InMemoryPostgresql.getInstance().public.query(
+      `update
+        "User"
+      set
+        username = '${data.username}',
+        email = '${data.email}',
+        contact = '${data.contact}',
+        avatar = '${data.avatar}',
+        preferred_currency = '${data.preferred_currency}'
+      where
+        "User".id = '${id}'`
+    )
+
+    return await InMemoryPostgresql.getInstance().public.one(`select * from "User" where "User".id = '${id}'`)
+  }
+}
+
+export default InMemoryUserRepo
