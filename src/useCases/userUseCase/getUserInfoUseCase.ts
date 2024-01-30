835e7f6c6b5cbf2004cb54dd20349bed5b0b9468
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-01-30 01:12:15 -0300

    feat: Created getUserInfoUseCase

---
+++ src/useCases/userUseCase/getUserInfoUseCase.ts
@@ -0,0 +1,27 @@
+import { User } from '@prisma/client'
+import IUserRepository from '@repository/IUserRepository'
+import UserNotFoundError from '@useCases/errors/UserNotFoundError'
+
+interface IGetUserInfoRequest {
+  userId: string
+}
+
+interface IGetUserInfoResponse {
+  user: User
+}
+
+class GetUserInfoUseCase {
+  constructor(private userRepo: IUserRepository) {}
+
+  async execute({ userId }: IGetUserInfoRequest): Promise<IGetUserInfoResponse> {
+    let dbUser = await this.userRepo.findById(userId)
+    if(!dbUser)
+      throw new UserNotFoundError()
+    
+    return {
+      user: dbUser
+    }
+  }
+}
+
+export default GetUserInfoUseCase
