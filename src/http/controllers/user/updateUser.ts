a175b0f807241f51745bc3349e76860da6308aa4
Mikael Marceniuk <48367519+MikaelMarceniuk@users.noreply.github.com>
2024-01-31 00:46:40 -0300

    improve: UpdateUserController params is now optional (#8)

--- src/http/controllers/user/updateUser.ts
+++ src/http/controllers/user/updateUser.ts
@@ -5,15 +5,17 @@ import { z } from 'zod'
 
 const updateUserController = async (req: FastifyRequest, rep: FastifyReply) => {
   const userSchema = z.object({
-    username: z.string(),
-    contact: z.string(),
-    avatar: z.string(),
-    preferred_currency: z.string()
+    username: z.string().optional(),
+    contact: z.string().optional(),
+    preferred_currency: z.string().optional()
   })
 
   const parsedBody = userSchema.parse(req.body)
 
-  await new updateUserUseCase(new PrismaUserRepo()).execute(req.user.id, parsedBody)
+  await new updateUserUseCase(new PrismaUserRepo()).execute({
+    userId: req.user.id,
+    data: parsedBody
+  })
 
   rep.statusCode = 204
   rep.send()
