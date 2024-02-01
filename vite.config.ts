51375bd476a31ca13866d6dc5b071f8467a622f0
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-02-01 03:37:33 -0300

    test: Add config for e2e tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
-import { defineConfig } from "vitest/config"
 import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
 
 export default defineConfig({
+  // @ts-ignore
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [["src/http/**", "prisma"]],
+  },
 })
